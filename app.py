fcb0c2cf5c81b7ddcc8829fde2b34e74a17eb79e
Chris6776-soli <christopherjossua@icloud.com>
2025-05-30 19:45:38 -0400

    02Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ nutritional_data = {
     "tilapia":     {"calories": 129, "protein": 26,  "fat": 2.7,  "carbohydrates": 0, "sodium": 56},
     "shrimp":      {"calories": 99,  "protein": 24,  "fat": 0.3,  "carbohydrates": 0.2, "sodium": 111},
 
-    "sweet_potato":  {"calories": 86,  "protein": 1.6, "fat": 0.1,  "carbohydrates": 20, "sodium": 55},
-    "white_potato":  {"calories": 77,  "protein": 2,   "fat": 0.1,  "carbohydrates": 17, "sodium": 6},
-    "red_potato":    {"calories": 70,  "protein": 1.9, "fat": 0.1,  "carbohydrates": 15.9, "sodium": 10},
-    "white_rice":    {"calories": 130, "protein": 2.7, "fat": 0.3,  "carbohydrates": 28, "sodium": 1},
+    "sweet potato":  {"calories": 86,  "protein": 1.6, "fat": 0.1,  "carbohydrates": 20, "sodium": 55},
+    "potato":  {"calories": 77,  "protein": 2,   "fat": 0.1,  "carbohydrates": 17, "sodium": 6},
+    "red potato":    {"calories": 70,  "protein": 1.9, "fat": 0.1,  "carbohydrates": 15.9, "sodium": 10},
+    "white rice":    {"calories": 130, "protein": 2.7, "fat": 0.3,  "carbohydrates": 28, "sodium": 1},
     "pasta":         {"calories": 160, "protein": 10,  "fat": 1.5,  "carbohydrates": 35, "sodium": 0},
     "quinoa":        {"calories": 120, "protein": 4.4, "fat": 1.9,  "carbohydrates": 21.3, "sodium": 7},
     "cauliflower":   {"calories": 25,  "protein": 1.9, "fat": 0.3,  "carbohydrates": 5, "sodium": 30},
@@ -20,10 +20,10 @@ nutritional_data = {
     "broccoli":      {"calories": 55,  "protein": 3.7, "fat": 0.6,  "carbohydrates": 11, "sodium": 33},
     "zucchini":      {"calories": 17,  "protein": 1.2, "fat": 0.3,  "carbohydrates": 3.1, "sodium": 8},
     "asparagus":     {"calories": 20,  "protein": 2.2, "fat": 0.2,  "carbohydrates": 3.9, "sodium": 2},
-    "brussels_sprouts": {"calories": 43, "protein": 3.4, "fat": 0.3, "carbohydrates": 9, "sodium": 25},
+    "brussels": {"calories": 43, "protein": 3.4, "fat": 0.3, "carbohydrates": 9, "sodium": 25},
     "spinach":       {"calories": 23,  "protein": 2.9, "fat": 0.4,  "carbohydrates": 3.6, "sodium": 79},
     "cucumber":      {"calories": 15,  "protein": 0.7, "fat": 0.1,  "carbohydrates": 3.6, "sodium": 2},
-    "green_beans":   {"calories": 31,  "protein": 1.8, "fat": 0.1,  "carbohydrates": 7, "sodium": 6},
+    "green beans":   {"calories": 31,  "protein": 1.8, "fat": 0.1,  "carbohydrates": 7, "sodium": 6},
 }
 
 def calculate_label(ingredients):
